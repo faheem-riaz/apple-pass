b6c1387c6c9d2abdcd32b8a21cfcd30cf9bc60e3
Aitebar Foundation <faheem.riaz.kn@gmail.com>
2023-05-07 20:57:07 +0500

    added pushToken varibale in the api

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from time import time
 from fastapi import FastAPI, __version__
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 @app.post('/apple-pass/v1/devices/{device_library_identifier}/registrations/{pass_type_identifier}/{serial_number}/')
-async def hello(device_library_identifier:str,pass_type_identifier:str,serial_number:str):
-    print(device_library_identifier)
+async def register(device_library_identifier:str,pass_type_identifier:str,serial_number:str,pushToken:str):
+    print("identifier",device_library_identifier)
+    print("pushToken ",pushToken)
     return {'device_library_identifier': device_library_identifier, 'pass_type_identifier': pass_type_identifier, "serial_number": serial_number}
 
 @app.post('/apple-pass/v1/log/')
