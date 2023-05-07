8aeb882f16c722e2af8a537da18368cc0bc5e6b1
Aitebar Foundation <faheem.riaz.kn@gmail.com>
2023-05-07 21:58:46 +0500

    updated logs and register api

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from time import time
-from fastapi import FastAPI, __version__
+from typing import List
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class DevicesPayload(BaseModel):
+    pushToken:str
+
+class LogsPayload(BaseModel):
+    logs:List[str]
+
 
 app = FastAPI()
 
 @app.post('/apple-pass/v1/devices/{device_library_identifier}/registrations/{pass_type_identifier}/{serial_number}/')
-async def register(device_library_identifier:str,pass_type_identifier:str,serial_number:str,pushToken:str):
+async def register(device_library_identifier:str,pass_type_identifier:str,serial_number:str,payload:DevicesPayload):
     print("identifier",device_library_identifier)
-    print("pushToken ",pushToken)
+    print("pushToken ",payload.pushToken)
     return {'device_library_identifier': device_library_identifier, 'pass_type_identifier': pass_type_identifier, "serial_number": serial_number}
 
 @app.post('/apple-pass/v1/log/')
-async def log():
-    print("logs")
+async def logs(payload:LogsPayload):
+    print("logs",payload.logs)
     return {'data': 'awesome'}
