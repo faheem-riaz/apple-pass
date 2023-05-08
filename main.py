19fb0d3cf242acde801ea037eeced0ada8568260
Aitebar Foundation <faheem.riaz.kn@gmail.com>
2023-05-08 12:14:33 +0500

    added new update api

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel
+from datetime import datetime
 
 class DevicesPayload(BaseModel):
     pushToken:str
@@ -11,20 +12,26 @@ class LogsPayload(BaseModel):
 
 app = FastAPI()
 
-@app.post('/apple-pass/v1/devices/{device_library_identifier}/registrations/{pass_type_identifier}/{serial_number}/')
+@app.post('/apple-pass/v1/devices/{device_library_identifier}/registrations/{pass_type_identifier}/{serial_number}')
 async def register(device_library_identifier:str,pass_type_identifier:str,serial_number:str,payload:DevicesPayload):
     print("identifier",device_library_identifier)
     print("pushToken ",payload.pushToken)
     return {'device_library_identifier': device_library_identifier, 'pass_type_identifier': pass_type_identifier, "serial_number": serial_number}
 
 
-@app.get('/apple-pass/v1/devices/{device_library_identifier}/registrations/{pass_type_identifier}/')
+@app.get('/apple-pass/v1/devices/{device_library_identifier}/registrations/{pass_type_identifier}')
 async def getSerialNumber(device_library_identifier:str,pass_type_identifier:str):
     print("serial number")
     return {"serial_number":"this is serial number"}
 
+@app.get('/apple-pass/v1/devices/{device_library_identifier}/registrations/{pass_type_identifier}')
+async def getSerialNumber(device_library_identifier:str,pass_type_identifier:str,passesUpdatedSince:str):
+    print("pass update since",passesUpdatedSince)
 
-@app.post('/apple-pass/v1/log/')
+    return {"lastUpdated": datetime.now,"serialNumbers":["E5982H-I2",]}
+
+
+@app.post('/apple-pass/v1/log')
 async def logs(payload:LogsPayload):
     print("logs",payload.logs)
     return {'data': 'awesome'}
